e109509d529aa7e204583acff9232171dfec25c1
Robert BJ HC <bj112143@gmail.com>
2016-05-29 22:25:48 -0500

    Modificar [cod Boleta/Factura] Agregado

--- app/Controller/VentaController.php
+++ app/Controller/VentaController.php
@@ -62,4 +62,60 @@
 
 			$this->view('Venta/Venta', $data);
 		}
+
+		public function modificar()
+		{
+			@session_start();
+
+			HS::sesion_no_iniciada($this);
+
+			$user = MUsuario::getEmail($_SESSION['user']['email'])[0];
+
+			$data = array(
+				"title"          => "Dist. Plas Anthony",
+				"mensaje"        => "",
+				"class_mensaje"  => "error",
+				"codigo" => "",
+				"cod_boleta" => "",
+				"cod_factura" => "",
+				"vendedores" => MVendedor::getAll(),
+				"usuario"        => $user,
+				'datos_template' => array(
+					"usuario" => $user,
+					'autor'   => 'Sistema',
+					'anio'    => '2016'
+				)
+			);
+
+			if(!empty($_POST) && isset($_POST))
+			{
+				$cod_boleta = $_POST['cod_boleta'] == "" ? NULL : $_POST['cod_boleta'];
+				$cod_factura = $_POST['cod_factura'] == "" ? NULL : $_POST['cod_factura'];
+
+				$venta = MVenta::getId($_POST['codigo']);
+
+				if(isset($venta))
+				{
+
+					if(MVenta::updateVenta($venta->getIdVenta(), $venta->getMonto(), $venta->getFkEmailUsuario(), $venta->getDniVendedor(), $cod_boleta, $cod_factura))
+					{
+						
+						$data['mensaje']              = "Venta Modificada Correctamente";					
+						$data['class_mensaje']        = "exito";	
+					}
+					else
+					{
+						$data['codigo']    = $_POST['codigo'];
+						$data['mensaje']  = "No se modifica la venta - Codigos ya registrados";
+					}
+				}
+				else
+				{
+						$data['codigo']    = $_POST['codigo'];
+						$data['mensaje']  = "No se encontro la venta con el código";
+				}
+			}
+
+			$this->view('Venta/Modificar', $data);
+		}
 	}
--- app/View/Venta/ModificarView.php
+++ app/View/Venta/ModificarView.php
@@ -23,10 +23,10 @@
 	<section>
 		<div class="ed-container" id="cuerpo">
 			<div class="ed-item tablet-50 centrar">	
-				<form class="formulario" action="<?= $helper->url('venta', 'nuevo');?>" method="post" name="registro">
+				<form class="formulario" action="<?= $helper->url('venta', 'modificar');?>" method="post" name="registro">
 				<div class="ed-container">
 					<div class="ed-item main-center cross-center">
-						<h1 class="formulario__item formulario__titulo">NUEVA VENTA</h1>
+						<h1 class="formulario__item formulario__titulo">MODIFICAR BOLETA/FACTURA VENTA</h1>
 					</div>
 					<div class="ed-item main-center cross-center">
 						<span class="formulario__item formulario__<?=@$class_mensaje?>"><?=@$mensaje?></span>
@@ -35,30 +35,13 @@
 						<input class="formulario__item formulario__dni base-100 tablet-50" name="codigo" type="text" placeholder="Codigo Proforma" value="<?=$codigo?>" required>
 					</div>
 					<div class="ed-item main-center cross-center">
-						<input class="formulario__item formulario__nombre base-100 tablet-50" name="monto" type="text" placeholder="Monto" value="<?=$monto?>" required>
-					</div>
-					<div class="ed-item main-center cross-center">
 						<input class="formulario__item formulario__nombre base-100 tablet-50" name="cod_boleta" type="text" placeholder="Codigo Boleta" value="<?=$cod_boleta?>" >
 					</div>
 					<div class="ed-item main-center cross-center">
 						<input class="formulario__item formulario__nombre base-100 tablet-50" name="cod_factura" type="text" placeholder="Codigo Factura" value="<?=$cod_factura?>" >
 					</div>
 					<div class="ed-item main-center cross-center">
-						<select class="formulario__item formulario__nombre base-100 tablet-50" name="vendedor" required>
-							<?php if( isset($vendedores)): ?>
-								<?php foreach($vendedores as $vendedor): ?>
-									<option value="<?=$vendedor->getDni()?>"><?=$vendedor->getNombre()?> <?=$vendedor->getApellido()?></option>
-								<?php endforeach; ?>
-							<?php else: ?>
-								<option value="null">No hay vendedores</option>
-							<?php endif; ?>
-						</select>
-					</div>
-					<div class="ed-item main-center cross-center">
-						<input class="formulario__item formulario__enviar base-100 tablet-50" type="submit" value="REGISTRAR VENTA">
-					</div>
-					<div class="ed-item main-center cross-center">
-						<a class="registrar" href="<?=$helper->url('venta', 'modificar')?>">Modificar Boleta/Factura</a>
+						<input class="formulario__item formulario__enviar base-100 tablet-50" type="submit" value="MODIFICAR VENTA">
 					</div>
 				</div>
 				</form>
